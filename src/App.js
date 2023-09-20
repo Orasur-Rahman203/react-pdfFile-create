566ab492c1c331e7383d8bf9cbea70ea0f98148b
Orasur Rahman <orasurrahman203@gmail.com>
2023-09-20 12:46:13 +0600

    UP

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from "react";
+import PDFFile from './Component/PDFFile.jsx';
+import { PDFDownloadLink } from "@react-pdf/renderer";
+// import ImagePicker from "./components/ImagePicker";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React PDF file download here!!!!!!</h1>
+      <PDFDownloadLink document={<PDFFile />} filename="FORM">
+      {({loading}) => (loading ? <button>Loading Document...</button> : <button>Download</button> )}
+      </PDFDownloadLink>
+      {/* <PDFFile /> */}
+    </div>
+  );
+};
+
+export default App;
